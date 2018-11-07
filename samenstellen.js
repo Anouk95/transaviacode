d56a3a023fc6d1eab3b23c6ae177320f043189a6
Anouk95 <43671399+Anouk95@users.noreply.github.com>
2018-11-07 17:32:21 +0100

    Add files via upload

--- samenstellen.js
+++ samenstellen.js
@@ -4,67 +4,85 @@
 
 /*eslint 'no-console': 0*/
 
-function enlarge1() {
-    if (document.getElementById('one').style.fontSize == "1em") {
-        document.getElementById('one').style.fontSize = "0em";
+var button = document.querySelector('.album li:nth-child(1) button');
+var change = 0;
+
+function bundle() {
+
+    if (change == 0) {
+        button.classList.add('addone');
+        change++;
     } else {
-        document.getElementById('one').style.fontSize = "1em";
+        button.classList.remove('addone');
+        change--;
     }
 }
-
-function enlarge2() {
-    if (document.getElementById('two').style.fontSize == "1em") {
-        document.getElementById('two').style.fontSize = "0em";
-    } else {
-        document.getElementById('two').style.fontSize = "1em";
-    }
-}
-
-function enlarge3() {
-    if (document.getElementById('three').style.fontSize == "1em") {
-        document.getElementById('three').style.fontSize = "0em";
-    } else {
-        document.getElementById('three').style.fontSize = "1em";
-    }
-}
+button.addEventListener('click', bundle);
 
 
-function enlarge4() {
-    if (document.getElementById('four').style.fontSize == "1em") {
-        document.getElementById('four').style.fontSize = "0em";
-    } else {
-        document.getElementById('four').style.fontSize = "1em";
-    }
-}
 
-function enlarge5() {
-    if (document.getElementById('five').style.fontSize == "1em") {
-        document.getElementById('five').style.fontSize = "0em";
-    } else {
-        document.getElementById('five').style.fontSize = "1em";
-    }
-}
 
-function enlarge6() {
-    if (document.getElementById('six').style.fontSize == "1em") {
-        document.getElementById('six').style.fontSize = "0em";
-    } else {
-        document.getElementById('six').style.fontSize = "1em";
-    }
-}
-
-function enlarge7() {
-    if (document.getElementById('seven').style.fontSize == "1em") {
-        document.getElementById('seven').style.fontSize = "0em";
-    } else {
-        document.getElementById('seven').style.fontSize = "1em";
-    }
-}
-
-function enlarge8() {
-    if (document.getElementById('eight').style.fontSize == "1em") {
-        document.getElementById('eight').style.fontSize = "0em";
-    } else {
-        document.getElementById('eight').style.fontSize = "1em";
-    }
-}
+//function enlarge1() {
+//    if (document.getElementById('one').style.fontSize == "1em") {
+//        document.getElementById('one').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('one').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge2() {
+//    if (document.getElementById('two').style.fontSize == "1em") {
+//        document.getElementById('two').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('two').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge3() {
+//    if (document.getElementById('three').style.fontSize == "1em") {
+//        document.getElementById('three').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('three').style.fontSize = "1em";
+//    }
+//}
+//
+//
+//function enlarge4() {
+//    if (document.getElementById('four').style.fontSize == "1em") {
+//        document.getElementById('four').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('four').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge5() {
+//    if (document.getElementById('five').style.fontSize == "1em") {
+//        document.getElementById('five').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('five').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge6() {
+//    if (document.getElementById('six').style.fontSize == "1em") {
+//        document.getElementById('six').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('six').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge7() {
+//    if (document.getElementById('seven').style.fontSize == "1em") {
+//        document.getElementById('seven').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('seven').style.fontSize = "1em";
+//    }
+//}
+//
+//function enlarge8() {
+//    if (document.getElementById('eight').style.fontSize == "1em") {
+//        document.getElementById('eight').style.fontSize = "0em";
+//    } else {
+//        document.getElementById('eight').style.fontSize = "1em";
+//    }
+//}
